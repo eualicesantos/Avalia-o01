a53f06e68f100a4d40db4fb3f286ee99a47f9e8e
eualicesantos <alicesantos@acad.ifma.edu.br>
2024-11-19 19:59:59 -0300

    Add files via upload

---
+++ cesar.js
@@ -0,0 +1,17 @@
+function criptografia(mensagem, chave, converter){
+    alfabeto = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n",
+                "o","p","q","r","s","t","u","v","w","x","y","z")
+
+    
+
+
+}
+
+function cifraLetra(letra, chave){
+    string
+
+}
+
+function decifraLetra(letra, chave){
+    string 
+} 
